cfc13f3c8c73fb4ebe9e0fe1d74b0c08716f08f0
ROYALTYPHAFOLI <boitshepo.phafoli@gmail.com>
2023-03-17 06:43:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the main funtion
+ * return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
