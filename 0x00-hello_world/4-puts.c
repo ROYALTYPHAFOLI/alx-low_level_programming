8da629329061a9dc9487c041f2505999007bb3c4
ROYALTYPHAFOLI <boitshepo.phafoli@gmail.com>
2023-03-17 07:12:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+
+
 /**
- * main - this is the main funtion
- * return: always 0
+ * main - prints "Programming is like building a multilingual
+ * puzzle followed by a new line.
+ * return: always 0.
  */
+
+
 int main(void)
+
+
 {
 	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
